3a41ebb3af60720663f32883e62b1b71389f0ff1
joeydelarago <joeydelarago@gmail.com>
2022-06-11 13:28:30 +0200

    Add game library searching

---
+++ game_library_seacher.py
@@ -0,0 +1,46 @@
+import os
+
+from typing import List
+
+
+class GameLibrarySearcher():
+    def __init__(self, library_path: str):
+        self.library_path = library_path
+        self.library = os.listdir(self.library_path)
+    
+    def search_text(self, query: str) -> List[str]:
+        if query in self.library:
+            # We found an exact match return it
+            return [query]
+        
+        matching = [match for match in self.library if query.lower() in match.lower()]
+        
+        top_ten = matching[:10]
+        
+        if len(matching) > 10:
+            top_ten.append("And " + str(len(matching) - 10) + " more...")
+        
+        return top_ten
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        
+        
+
+        
+        
+        
+    
+    
+    
+    
+    
