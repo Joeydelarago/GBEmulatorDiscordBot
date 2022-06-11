59edc29e3abaee97146d5c26e7e984c747930574
nanorh <nanorh@users.noreply.github.com>
2022-06-11 11:49:10 +0100

    Fixed indentation

--- src/cogs/emulator.py
+++ src/cogs/emulator.py
@@ -84,16 +84,16 @@ class ImageBuffer():
         # Fill the buffer to avoid assigning extra memory
         self.buffer = [None] * size
     
-def push(self, item) -> None:
-    self.buffer[self.index] = item
-    # Increase the index by 1, wrap back to 0 when index > buffer size
-    self.index = (self.index + 1) % self.size
-    
-def get_all(self) -> List:
-    # Return everything in buffer in order starting with item at buffer_index
-    all = self.buffer[self.index:] + self.buffer[0:self.index]
-    all = [frame for frame in all if frame is not None]
-    return 
+    def push(self, item) -> None:
+        self.buffer[self.index] = item
+        # Increase the index by 1, wrap back to 0 when index > buffer size
+        self.index = (self.index + 1) % self.size
+        
+    def get_all(self) -> List:
+        # Return everything in buffer in order starting with item at buffer_index
+        all = self.buffer[self.index:] + self.buffer[0:self.index]
+        all = [frame for frame in all if frame is not None]
+        return 
     
 
 def setup(client: commands.Bot):
