01d7bae1590fc15ce39291f58e1825783b5a1e84
joeydelarago <joeydelarago@gmail.com>
2022-06-11 22:20:41 +0200

    Add a proper initialization function and closing function.
    Give more responsibility to GameLibrary class, it will now manage unzipping zipped roms as well as searching.

--- src/cogs/emulator.py
+++ src/cogs/emulator.py
@@ -1,14 +1,11 @@
 import logging
 import os
-from logging import Logger
-from typing import List
-from PIL.Image import Image
 from pyboy import PyBoy
 from pyboy.utils import WindowEvent
 from discord.ext import commands
 
 import discord.embeds
-from src.game_library_seacher import GameLibrarySearcher
+from src.game_library import GameLibrary
 from src.gif_exporter import GifExporter
 from src.image_buffer import ImageBuffer
 
@@ -23,6 +20,8 @@ class Emulator(commands.Cog):
         self.roms_path = os.getcwd() + "/roms"
         self.current_rom_name = ""
         self.save_slot = 0
+        #  0 is fast as possible
+        self.emulation_speed = 0
 
         # Initialize emulator screenshot buffer, each second is 60 frames
         self.buffer_seconds = 6  # Buffer size in seconds
@@ -30,48 +29,51 @@ class Emulator(commands.Cog):
         
         self.gif_exporter = GifExporter()
         
-        self.library_searcher = GameLibrarySearcher(self.roms_path)
+        self.library = GameLibrary(self.roms_path)
 
-        self.initialize_game("pokemon-red.gb")
+        self.initialize_game("pokemon-red", self.roms_path + "/" + "pokemon-red.gb")
         
     def tick(self, tick_count: int) -> None:
         for t in range(tick_count):
             self.pyboy.tick()
             self.image_buffer.push(self.pyboy.screen_image())
-            
+
+    def initialize_game(self, rom_name: str, rom_path: str, save_slot: int = 0) -> None:
+        """ Load up new rom and load state """
+        self.current_rom_name = rom_name
+        self.pyboy = PyBoy(rom_path)
+        self.pyboy.set_emulation_speed(self.emulation_speed)
+        self.save_slot = save_slot
+        self.load_state()
+
+    def close_game(self):
+        """ Shutdown pyboy and save game state """
+        if self.pyboy:
+            self.save_state()
+            self.pyboy.stop()
+
+            #  This will remove the pyboy from memory
+            self.pyboy = None
+
     def export_buffer_as_gif(self):
         self.gif_exporter.create_gif(self.image_buffer.get_all())
-        
+
     def save_state(self) -> None:
-        save_name = "{rom_name}_{save_slot}.state".format(rom_name=self.current_rom_name, save_slot=self.save_slot)
+        save_name = f"{self.current_rom_name}_{self.save_slot}.state"
         with open(self.saves_path + "/" + save_name, "wb") as save_file:
             self.pyboy.save_state(save_file)
 
     def load_state(self) -> None:
-        save_name = "{rom_name}_{save_slot}.state".format(rom_name=self.current_rom_name, save_slot=self.save_slot)
+        save_name = f"{self.current_rom_name}_{self.save_slot}.state"
         save_path = self.saves_path + "/" + save_name
 
         if not os.path.exists(save_path):
             logging.warning("There is no save file to load at path: " + save_path)
             return
 
-        with open(self.save_path + "/" + save_name, "rb") as save_file:
+        with open(self.saves_path + "/" + save_name, "rb") as save_file:
             self.pyboy.load_state(save_file)
 
-    def initialize_game(self, rom_name: str, save_slot: int = 0) -> None:
-        self.current_rom_name = rom_name
-        self.pyboy = PyBoy(self.roms_path + "/" + rom_name)
-        self.save_slot = save_slot
-        self.load_state()
-
-    def close_game(self):
-        if self.pyboy:
-            self.pyboy.save_state()
-            self.pyboy.stop()
-
-            #  This will remove the pyboy from memory
-            self.pyboy = None
-    
     def move(self, move):
         if move == 'up':
             self.pyboy.send_input(WindowEvent.PRESS_ARROW_UP)
@@ -106,23 +108,26 @@ class Emulator(commands.Cog):
             self.tick(25)
             self.pyboy.send_input(WindowEvent.RELEASE_BUTTON_SELECT)
 
+
     @commands.Command
     async def load(self, ctx, query: str):
-        search_results = self.library_searcher.search_text(query)
+        search_results = self.library.search_text(query)
         if len(search_results) == 1:
             self.close_game()
-            self.initialize_game(search_results[0])
+            rom_name = search_results[0]
+            rom_path = self.library.get_game_path(rom_name)
+            self.initialize_game(rom_name, rom_path)
         else:
             #print some search options in chat
             pass
 
     @commands.Command
+    async def tick_test(self, ctx, tick_count: int = 60):
+        self.tick(tick_count)
+
+    @commands.Command
     async def newgame(self, ctx):
-        self.pyboy = PyBoy(self.rom_path)
-
-        # This will try to emulate as fast as possible
-        self.pyboy.set_emulation_speed(0)
-
+        self.initialize_game("pokemon-red", self.roms_path + "/" + "pokemon-red.gb")
         self.tick(self.buffer_seconds * 60)
         self.export_buffer_as_gif()
         self.save_state()
@@ -130,11 +135,7 @@ class Emulator(commands.Cog):
 
     @commands.Command
     async def gba(self, ctx, move, amount: int = 1):
-        self.pyboy = PyBoy(self.rom_path)
-
-        # This will try to emulate as fast as possible
-        self.pyboy.set_emulation_speed(0)
-        self.load_state()
+        self.initialize_game("pokemon-red", self.roms_path + "/" + "pokemon-red.gb")
         self.tick(20)
         for i in range(amount):
             self.move(move)
